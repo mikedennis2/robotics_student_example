8e32c0c671dc350d4d3ff024f40d562c64ecbee7
jose m <jmcwaggy101@gmail.com>
2020-11-12 19:33:34 -0500

    Attendance Cost
    - Added calculate attendance cost method to the student class.

--- src/main/java/highschool/CristoRey.java
+++ src/main/java/highschool/CristoRey.java
@@ -17,8 +17,8 @@ public class CristoRey {
 
         double rate_per_credit_hour = 550.75;
 
-        double student_a_fees = student_a_credit_hours * rate_per_credit_hour;
-        double student_b_fees = student_b_credit_hours * rate_per_credit_hour;
+        double student_a_fees = Student.calculateAttendenceCost(rate_per_credit_hour, student_a_credit_hours);
+        double student_b_fees = Student.calculateAttendenceCost(rate_per_credit_hour, student_b_credit_hours);
 
         System.out.println("Student a's name is: " + student_a_name + " and he is " + student_a_age + ".");
         System.out.println("Student a's total cost of attendance is: $" + student_a_fees + ".");
--- src/main/java/highschool/Student.java
+++ src/main/java/highschool/Student.java
@@ -6,9 +6,9 @@ public class Student {
     }
 
     // We need to define the function below and use it in CristoRey.java
-    // public static double calculateAttenceCost(){
-    //     return 
-    // }
+    public static double calculateAttendenceCost(double rate_per_credit_hour, int number_of_credit_hours){
+        return rate_per_credit_hour * number_of_credit_hours;
+    }
 
 
 }
