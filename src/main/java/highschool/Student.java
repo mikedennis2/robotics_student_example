01ec2313a93373b5ed72e3b02d0d659f4dae7e5c
JacobSolano <jsolano.2021@detroitcristorey.org>
2020-11-12 19:42:27 -0500

    configured student class

--- src/main/java/highschool/Student.java
+++ src/main/java/highschool/Student.java
@@ -1,8 +1,23 @@
 package highschool;
 
 public class Student {
+
+    String name;
+    int age;
+    int credit_hours;
+
     public Student(){
-        // Nothing in here yet
+
+    }
+
+
+    public Student(String name, int age, int credit_hours){
+
+        this.name = name;
+        this.age = age;
+        this.credit_hours = credit_hours;
+     
+
     }
 
     // We need to define the function below and use it in CristoRey.java
